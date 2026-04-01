4cba06aa024bef771bd3facdd484dc57b0b1899e
DeemTempus <deem.temp99@gmail.com>
2026-04-01 11:12:57 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,23 +1,30 @@
 const sectionSelect = document.getElementById("section");
 const gender = document.getElementById("gender");
-const contactInput = document.getElementById("contact");
+document.addEventListener("DOMContentLoaded", () => {
 
-contactInput.addEventListener("input", (e) =>
-    {
+    const contactInput = document.getElementById("contact");
+
+    contactInput.addEventListener("input", (e) => {
         let numbers = e.target.value.replace(/\D/g, "").substring(0, 10);
+
         let formatted = "";
 
-        if (numbers.length > 0){
-            formatted += "(" + numbers.string(0,3);
+        if (numbers.length > 0) {
+            formatted = "(" + numbers.substring(0, 3);
         }
-        if (numbers.length >= 4){
-            formatted += ")" + numbers.string(3,6);
+        if (numbers.length >= 4) {
+            formatted = "(" + numbers.substring(0, 3) + ") " + numbers.substring(3, 6);
         }
-        if (numbers.length >= 7){
-            formatted += "-" + numbers.string(6,10);
+        if (numbers.length >= 7) {
+            formatted = "(" + numbers.substring(0, 3) + ") " +
+                        numbers.substring(3, 6) + "-" +
+                        numbers.substring(6, 10);
         }
+
         e.target.value = formatted;
     });
+
+});
 
 async function loadSections() {
     const res = await fetch("/sections");
